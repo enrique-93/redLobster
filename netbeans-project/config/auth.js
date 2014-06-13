102ab4a16ecd32a09ba93e071bf85007c2414266
root <root@onlynode.onlynode.d1.internal.cloudapp.net>
2014-06-13 01:27:15 +0000

    quitamos el auth y el node_modules de las actualizaciones. y ya...

--- netbeans-project/config/auth.js
+++ netbeans-project/config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': '646551288753807', // your App ID
         'clientSecret': 'ff7caae0cd9e08ed2b2a1733f16c7de5', // your App Secret
-        'callbackURL': 'http://localhost:80/auth/facebook/callback'
+        'callbackURL': 'http://pescaredlobster.com/auth/facebook/callback'
     }
 };
 
